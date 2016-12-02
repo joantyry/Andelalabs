b0bfce683f7a00ecdc76e75a2b2efbbf5eae541a
joantyry <tyry7979@gmail.com>
2016-12-02 11:43:11 +0300

    Add files via upload
    For example for the input "olly olly in come free"
    
    olly: 2
    in: 1
    come: 1
    free: 1

---
+++ wordcount.py
@@ -0,0 +1,16 @@
+def words(mylst):
+  mylst2 = {}
+  for word in list(mylst.split()):
+    
+    try:
+      word_count = int(word)
+    except ValueError:
+
+      mylst2[word] = mylst.split().count(word)
+
+    else:
+
+      mylst2[int(word)] = mylst.split().count(word)
+
+  return mylst2
+    
